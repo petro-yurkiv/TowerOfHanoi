422cd9d21f1b14157d7f966cd63118fb282e7a86
Ptero Yurkiv <petroyur12@gmail.com>
2024-06-07 02:00:13 -0700

    Initial Commit

---
+++ Tower of Hanoi/Tower_of_HanoiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tower_of_HanoiApp.swift
+//  Tower of Hanoi
+//
+//  Created by Petro Yurkiv on 07.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tower_of_HanoiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
